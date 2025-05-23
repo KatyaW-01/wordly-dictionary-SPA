ccc8078208ae45856ca75f9eb3a1d6004cc2de5b
Katya Weicht <katya.weicht@gmail.com>
2025-05-23 14:15:46 -0600

    Write fetch dictionary data function

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
-//javacript goes here
+async function fetchDictionaryData(word) {
+  if(typeof word !== "string" || word.trim() === "") {
+    displayError('Error: Please enter a valid word')
+    return; //try block wont run if there is an error
+  }
+  const trimmedWord = word.trim() //accounts for if user puts extra spaces at beginning or end of the word
+  try {
+    const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${trimmedWord}`)
+    if (!response.ok) {
+      displayError('Error: Word not found')
+      return; //stop rest of function from running
+    }
+    const data = await response.json()
+    displayDictionaryData(data)
+  } catch (error) {
+    displayError('Failed to load word data. Please try again.')
+    console.error('Error fetching word data:', error)
+  }
+}
 
-const api = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+function displayDictionaryData(data) {
+
+}
+
+function displayError(message) {
+
+}
